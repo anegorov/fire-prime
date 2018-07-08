25d9ca4b2db1c3a066b1bba0bb1cd9bea8c6422c
Andrey Egorov <a-n-egorov@yandex.ru>
2018-07-08 22:42:03 +0300

    1 delete carousel. 2 add buttons to main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireStorageModule } from 'angularfire2/storage';
 
 import { environment } from '../environments/environment';
 import { ListComponent } from './list/list.component';
